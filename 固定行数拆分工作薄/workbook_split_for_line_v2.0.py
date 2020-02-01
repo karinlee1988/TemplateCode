12d4c649247ac8175dbfcb91ce6296ffac837114
karinlee <263384909@qq.com>
2020-02-01 10:35:39 +0800

    workbook_split_for_line_v2.0.py 新增注释，优化代码

--- 固定行数拆分工作薄/workbook_split_for_line_v2.0.py
+++ 固定行数拆分工作薄/workbook_split_for_line_v2.0.py
@@ -11,19 +11,27 @@ import tkinter as tk
 import tkinter.filedialog
 
 class WorkbookSplitRegularLine(object):
+    """
+    按照固定行数将单个工作薄拆分为多个工作薄
+    表头默认为1行
+    """
 
     def __init__(self,full_filename,batche_num):
+        """
 
+        :param full_filename: 文件的全文件名（相对路径或绝对路径）
+        :param batche_num: 需要拆分的行数
+        :type full_filename: str
+        :type batche_num: int or str
+        """
         self.full_filename = full_filename
         self.batche_num = batche_num
+        # 获取self.workbook 要拆分的工作薄对象
         self.workbook = openpyxl.load_workbook(self.full_filename)
-        # 得到所有worksheet
-
-        # 获取第一个worksheet
+        # 获取worksheet，默认为第一个sheet用于拆分
         self.sheet = self.workbook[self.workbook.sheetnames[0]]
         # 把生成器转换为列表
         self.lines = list(self.sheet.rows)
-
         # 获取第一行合并行
         # first_line = lines[0]
         # 获取表头字段行
@@ -31,11 +39,19 @@ class WorkbookSplitRegularLine(object):
         # 获取数据行
         self.dataline = self.lines[1:]
 
-    # 把一个数据集保存成一个xlsx文件，参数1：数据集，参数2：拆分文件的流水号。
     def one_sheet(self,data, sheet_no):
+        """
+        把一个数据集保存成一个xlsx文件，。
+        :param data: 数据集
+        :param sheet_no: 拆分文件的流水号
+        :type data: list
+        :type sheet_no: int
+        :return: None
+        """
         # 创建一个xlsx文件对象
         wb = openpyxl.Workbook()
-        ws = wb.active    # 取得默认的worksheet
+        # 取得默认的worksheet
+        ws = wb.active
         # ws.title = '新测试表%02d' % (sheet_no+1)   # 设置一个标题
         # # 第一行写入合并行
         # ws.cell(row=1, column=1).value = first_line[0].value
@@ -45,24 +61,21 @@ class WorkbookSplitRegularLine(object):
         header_idx = 0
         for col in self.header[0]:
             ws.cell(row=1, column=(header_idx + 1)).value = col.value
-            # print(col.value)
             header_idx += 1
-
-        row_idx = 0   # 纪录行索引
+        # 纪录行索引
+        row_idx = 0
         for row_ in data:
             col_idx = 0
             for col in row_:
-                ws.cell(row=(row_idx+2) ,column=(col_idx + 1)).value = col.value    # 数据行行从2开始
-                col_idx += 1    # 纪录列索引
+                # 数据行行从2开始
+                ws.cell(row=(row_idx+2) ,column=(col_idx + 1)).value = col.value
+                # 纪录列索引
+                col_idx += 1
             row_idx += 1
-
         # 保存文件
         wb.save(self.full_filename+'-%02d.xlsx' % (sheet_no + 1))
 
     def dealing(self):
-
-        # 每个文件数据行数
-
         # 计算拆分文件个数
         batches = (len(self.dataline) // self.batche_num) + 1
         # 循环写数据集到每一个文件
@@ -71,13 +84,10 @@ class WorkbookSplitRegularLine(object):
             lines_ = self.dataline[pt * self.batche_num : (pt + 1) * self.batche_num]
             self.one_sheet(lines_,pt)
 
-
-
 class WorkbookSplitRegularLineGUI(object):
     """
     tkinter GUI界面模板（可选择文件进行操作）
     """
-
     def __init__(self):
         """
         创建界面
@@ -85,9 +95,7 @@ class WorkbookSplitRegularLineGUI(object):
         # 新建窗口
         self.master = tk.Tk()
         self.photo = tk.PhotoImage(file="img\\si2.gif")
-        # self.path 用于存放选择的文件路径
-
-        # self.v1 self.v2接收用户参数
+        # self.path 用于存放选择的文件路径,self.v1 self.v2接收用户参数
         # 注意！这些都是tk.StringVar()对象，不是str，其他地方要用的话要用get()方法获取str
         self.path = tk.StringVar()
         self.flag = tk.StringVar()
@@ -105,11 +113,10 @@ class WorkbookSplitRegularLineGUI(object):
     def img_pack(self):
         """
         窗口图片横幅
-        :return:
-        :rtype:
+        :return: None
+        :rtype:  None
         """
         # 图片贴上去
-
         img_lable = tk.Label(self.master, image=self.photo)
         img_lable.pack()
 
@@ -121,10 +128,6 @@ class WorkbookSplitRegularLineGUI(object):
         """
         self.master.geometry("800x600+600+100")
         self.master.title("固定行数拆分excel工作薄工具by李加林v2.0")
-        # 图片贴上去
-        # self.photo = tk.PhotoImage(file="xzpq.gif")
-        # imgLable = tk.Label(self.master, image=self.photo)
-        # imgLable.pack()
         # 把标题贴上去
         tk.Label(self.master, text="固定行数拆分xlsx单个工作薄为多个工作薄\n", font=("黑体", 20)).pack()
         tk.Label(self.master, text="表头默认为1行\n\n", font=("黑体", 16)).pack()
@@ -132,8 +135,8 @@ class WorkbookSplitRegularLineGUI(object):
     def frame(self):
         """
         框架布局设置
-        :return:
-        :rtype:
+        :return: None
+        :rtype: None
         """
         # 框架贴上去，再在框架里添加Lable，Entry，Button等控件
         frame1 = tk.Frame(self.master)
@@ -144,7 +147,6 @@ class WorkbookSplitRegularLineGUI(object):
         tk.Button(frame1, text="路径选择", command=self.select_path, font=("黑体", 16)).grid(row=1, column=2)
         tk.Label(frame1, text="请输入固定拆分行数：", font=("黑体", 16)).grid(row=2, column=0)
         tk.Entry(frame1, textvariable=self.line).grid(row=2, column=1)
-
         # 按这个按钮执行主程序
         tk.Button(frame1, text="开始处理", command=self.main, font=("黑体", 16)).grid(row=4, column=1, pady=66)
         tk.Entry(frame1, textvariable=self.flag, state="readonly",width=20).grid(row=4, column=2, pady=66)
@@ -152,8 +154,8 @@ class WorkbookSplitRegularLineGUI(object):
     def select_path(self):
         """
         选择文件，并获取文件的绝对路径
-        :return:
-        :rtype:
+        :return: None
+        :rtype:  None
         """
         # 选择文件，path_select变量接收文件地址
         path_select = tkinter.filedialog.askopenfilename()
@@ -166,14 +168,14 @@ class WorkbookSplitRegularLineGUI(object):
     def main(self):
         """
         这个是主程序
-        :return:
-        :rtype:
+        :return: None
+        :rtype:  None
         """
-
         run = WorkbookSplitRegularLine(self.path.get(),int(self.line.get()))
         run.dealing()
         # 标志设置为处理完成
         self.flag.set("处理完成！")
 
 if __name__ == '__main__':
+    # 实例化gui开始运行
     app = WorkbookSplitRegularLineGUI()
