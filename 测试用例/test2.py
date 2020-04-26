f2219a58bda4f8363894e2d4dc70cb0fcf0e3054
karinlee <263384909@qq.com>
2020-04-26 11:50:36 +0800

    add test2.py

---
+++ 测试用例/test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/25 12:22
+# @Author : karinlee
+# @FileName : test2.py.py
+# @Software : PyCharm
+# @Blog : https://blog.csdn.net/weixin_43972976
+
+
+def getdict(workbook):
+    pass
